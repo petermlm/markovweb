46d9fe62dbc8f789d12322bdc27b051c33be812c
Pedro Melgueira <pedromelgueira@gmail.com>
2019-03-26 18:59:15 +0100

    Adds details to vue config

--- markov-web/vue.config.js
+++ markov-web/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/markov/'
-    : '/'
+    : '/',
+
+  chainWebpack: (config) => {
+    config.plugin('html').init((Plugin, args) => {
+      const newArgs = {
+        ...args[0],
+      };
+      newArgs.minify.removeAttributeQuotes = false;
+      return new Plugin(newArgs);
+    });
+  },
 }
